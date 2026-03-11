da3f4075a4c4579950b637a9ad29fb749356a588
24122013 <24122013@student.hcmus.edu.vn>
2026-03-11 14:02:52 +0700

    Add solution for softmax-function

---
+++ softmax-function/softmax-function.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+def softmax(x):
+    x = np.asanyarray(x)
+    
+    max_x = np.max(x, axis=-1, keepdims=True)
+    
+    exp_x = np.exp(x - max_x)
+    
+    return exp_x / np.sum(exp_x, axis=-1, keepdims=True)
